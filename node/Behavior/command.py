f9b2a3d46765e8f82ffa07873d230b21b6211fc3
stephanoumenos <s_dechecchi@hotmail.com>
2019-11-13 11:14:59 +0100

    Fixed queue loop

--- node/Behavior/command.py
+++ node/Behavior/command.py
@@ -5,6 +5,8 @@ from st5drone.msg import BehaviorStatus
 from std_msgs.msg import String
 from event_queue import EventQueue
 
+from multiprocessing import Lock
+
 import sched, time
 
 class Command:
@@ -21,6 +23,8 @@ class Command:
         self.command = rospy.Subscriber("/command", String, self.command_callback, queue_size=1)
         self.behavior = rospy.Publisher("/behavior", BehaviorStatus, queue_size=1)
 
+        self.queue_lock = Lock()
+
         self.event_queue = EventQueue()
 
     def command_callback(self, msg):
@@ -32,16 +36,9 @@ class Command:
             # print(message)
             self.behavior.publish(message)
 
-        self.event_queue.setEvents(self.commands[msg.data])
+        with self.queue_lock:
+            self.event_queue.setEvents(self.commands[msg.data])
 
-        while not rospy.is_shutdown():
-            validEvents = self.event_queue.returnValidEvents()
-            
-            for behavior in validEvents:
-                self.contact_behavior(behavior[1])
-
-            rospy.sleep(0.1)
-        
     def contact_behavior(self, behavior_name):
     
         message = BehaviorStatus()
@@ -49,6 +46,16 @@ class Command:
         message.active = True
 
         self.behavior.publish(message)
+
+    def loop(self):
+        while not rospy.is_shutdown():
+            with self.queue_lock:
+                validEvents = self.event_queue.returnValidEvents()
+
+            for behavior in validEvents:
+                self.contact_behavior(behavior[1])
+
+            rospy.sleep(0.1)
 
 
 if __name__ == '__main__':     # This is the main thread, thread #1
@@ -60,8 +67,8 @@ if __name__ == '__main__':     # This is the main thread, thread #1
     # rospy.sleep(1)
     # my_node.command_callback("Dance")
     
-    #my_node.loop()
-    rospy.spin() # useless... since loop already blocks. If you have
+    my_node.loop()
+    #rospy.spin() # useless... since loop already blocks. If you have
                  # no idle job (i.e. loop) to do outside event
                  # handling, rospy.spin() is mandatory in order to
                  # prevent from immediate termination of your node.
