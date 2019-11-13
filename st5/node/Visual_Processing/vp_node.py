b814cab50e56cd60737295fdcbb65e05b1204bde
João Gabriel Lopes <jglopes6@gmail.com>
2019-11-13 15:19:02 +0100

    Establishing I/O out of node

--- st5/node/Visual_Processing/vp_node.py
+++ st5/node/Visual_Processing/vp_node.py
@@ -11,7 +11,8 @@ import vp_core as vp
 class Test:
     
     def __init__(self):
-        self.sub = rospy.Subscriber("bebop/image_raw/compressed",  CompressedImage, self.on_image,  queue_size = 1, buff_size=2**22)
+        self.sub = rospy.Subscriber("image_in/compressed",  CompressedImage, self.on_image,  queue_size = 1, buff_size=2**22)
+        self.pub = rospy.Publisher("image_out/compressed", CompressedImage, queue_size=1)
         
     def on_image(self, msg):
         compressed_in = np.fromstring(msg.data, np.uint8)
@@ -22,7 +23,18 @@ class Test:
         # time sequence when the images come from a ros bag, since ros bag execution
         # can be paused, run step by step, ...
         now = msg.header.stamp
-        vp.draw(frame, now)
+
+        # Let us publish the gray image
+        if self.pub.get_num_connections() > 0:
+            #image = vp.io_test(frame, now)
+
+            msg              = CompressedImage()
+            msg.header.stamp = rospy.Time.now()
+            msg.format       = "jpeg"
+            msg.data         = np.array(cv2.imencode('.jpg', frame)[1]).tostring()
+            self.pub.publish(msg)
+
+
 
 if __name__ == '__main__':
     rospy.init_node('test')
