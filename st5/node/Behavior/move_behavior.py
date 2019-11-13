d6ef65cd35221995804c949d338ceabc18aa04cd
stephanoumenos <s_dechecchi@hotmail.com>
2019-11-13 17:36:10 +0100

    Make drone always hover after movements and after a switch to turn off the controller if the drone is hovering

--- st5/node/Behavior/move_behavior.py
+++ st5/node/Behavior/move_behavior.py
@@ -5,7 +5,7 @@
 import rospy
 from behavior import Behavior
 
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, String
 
 
 class Move(Behavior):
@@ -22,12 +22,16 @@ class Move(Behavior):
         if backwards:
             self.movement_speed *= -1
         self.pub = rospy.Publisher(topic, Float32, queue_size=0)
+        self.pub_command = rospy.Publisher('/command', String, queue_size=0)
 
     def move(self):
         self.pub.publish(self.movement_speed)
 
     def stop(self):
         self.pub.publish(0)
+        hover_msg = String()
+        hover_msg.data = "hover"
+        self.pub_hover.publish(hover_msg)
 
     def on_status_on(self):
         self.move()
